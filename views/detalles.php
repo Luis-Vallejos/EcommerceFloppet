1856067fd7b949957c99dac8295fd9f08145219e
Luis Vallejos <vallejosevangelistaluis@gmail.com>
2025-06-25 18:05:26 -0500

    feat(detalles): agregar vista de detalles con encabezado, barra de navegación y pie de página

---
+++ views/detalles.php
@@ -0,0 +1,13 @@
+<?php 
+session_start();
+require_once __DIR__ . '/../includes/header.php';
+require_once __DIR__ . '/../includes/navbar.php';
+?>
+
+
+
+
+<?php 
+require_once __DIR__ . '/../includes/footer.php';
+require_once __DIR__ . '/../includes/scripts.php';
+?>
